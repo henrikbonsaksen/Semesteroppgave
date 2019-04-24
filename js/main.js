898feb329e45451c978b1d6b6cc4de0f25bfed66
henrikbonsaksen <henrik_bon@hotmail.com>
2019-04-24 13:54:31 +0200

    yez

--- js/main.js
+++ js/main.js
@@ -9,35 +9,59 @@ let utdanning_wildboy = "http://wildboy.uib.no/~tpe056/folk/85432.json";
 let befolkning_wildboy = "http://wildboy.uib.no/~tpe056/folk/104857.json";
 let sysselsatte_wildboy = "http://wildboy.uib.no/~tpe056/folk/100145.json";
 
-// HINT: dot otation eller bracket notation
 
-let utd = new Population(utdanning_wildboy);
+// HINT: dot notation eller bracket notation
+// funksjon for å hente JSON fra lab//
+function loadJSON(url, elmntid) {
+  var url = url;
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      var json = (JSON.parse(xhttp.responseText));
+      byer = []
+      for (by in json.elementer) {
+        // document.createElement(document.getElementById(elmntid).innerHTML = ("<td>"));
+
+        document.getElementById(elmntid).innerHTML = by;
+        byer.push(by);
+      };
+      // document.getElementById(elmntid).innerHTML =
+      // xhttp.responseText;
+    }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+};
+
+// let utd = new Population(utdanning_wildboy);
+let obj = new Population(befolkning_wildboy);
 
 // Konstruktøren skal defineres med (minst) et parameter: datasettets URL. Dersom dere finner det hensiktsmessig,
 // kan dere også definere den med ytterligere parametre. Objektet som returneres skal i det minste ha følgende metoder:
 function Population(url) {
   this.url = url;
-  loadJSON(url, getNames,'jsonp');
+  getNames();
 
   // getNames returnerer listen av alle kommunenavnene (som de fremtrer i datasettet).
-  function getNames(url) {
-    let keyIndex = Object.keys(url)
-    let kommunenavn = url[0];
-    console.log(kommunenavn);
+  function getNames() {
+    var navn = loadJSON(url, "navn");
   }
 
   // getIDs returnerer listen av alle kommunenummerene.
-  function getIDs(url) {
-    let kommuneID = url[0];
-    console.log(kommuneID);
+  function getIDs() {
+    let kommuneID = loadJSON(url)[0];
+    // console.log(kommuneID);
   }
 
-  function getInfo(url) {
+  function getInfo() {
     console.log("Ferdig");
     // let page = Object.keys(data.query.pages) [0];
     // console.log(page);
   }
 };
+
+
+
 
 // Merk at et kommunenummer er fire heltall, og Haldens
 // kommunenummer er “0101”, altså inkludert et ledende 0. Det kan derfor være lurt å representere kommunenummer
@@ -54,21 +78,6 @@ function Population(url) {
 // let befolkning = new Population(befolkning_wildboy);
 // let sysselsatte = new Population(sysselsatte_wildboy);
 
-
-// funksjon for å hente JSON fra lab//
-function loadJSON(url, elmntid) {
-  var url = url;
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (xhttp.readyState == 4 && xhttp.status == 200) {
-      console.log(JSON.parse(xhttp.responseText));
-      document.getElementById(elmntid).innerHTML =
-      xhttp.responseText;
-    }
-  };
-  xhttp.open("GET", url, true);
-  xhttp.send();
-};
 
 // vise-og-skjule-knapp
 function showBox(id) {
@@ -88,25 +97,23 @@ function showBox(id) {
 };
 
 
-// function openBox(boxType) {
+// function showBox(boxType) {
 //   var box = document.getElementById(boxType);
 //   var boxes = document.getElementsByClassName("infoBox");
-//   var navElements = document.getElementByTagName("nav")[0].firstElementchild.children;
+//   var navElements = document.getElementByTagName("navbar")[0].firstElementchild.children;
 //   for(var i = 0; i < boxes.lenght; i++){
 //     boxes[i].classList.remove("showBox");
 //   }
 //   if(box.classList.contains("showBox") === false) {
 //     box.classList.add("showBox");
 //     for(var i = 0; i < navElements.lenght; i++) {
-//       navElements[i].classList.remove("activeNav");
+//       navElements[i].classList.remove("show");
 //       if(navElements[i].innerHTML === boxType) {
-//         navElements[i].classList.add("activeNav");
-
-
-
-
-
-
+//         navElements[i].classList.add("show");
+//       }
+//     }
+//   }
+// }
 
 
 
